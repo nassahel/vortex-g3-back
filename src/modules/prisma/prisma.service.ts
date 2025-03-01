61a0fc7ede97dc87dd891492580fd30d2daa430b
nassahel <nassahel.elias@gmail.com>
2025-03-01 03:38:29 -0300

    fixing deploy 13

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,24 +1,36 @@
-import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
+// import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+//   private readonly logger = new Logger(PrismaService.name);
+
+//   async onModuleInit() {
+//     this.logger.log('📡 Intentando conectar a la base de datos...');
+
+//     try {
+//       await this.$connect();
+//       this.logger.log('✅ Conexión exitosa a la base de datos.');
+//     } catch (error) {
+//       this.logger.error('❌ Error al conectar a la base de datos:', error);
+//       throw error; // Para que el deploy falle y te avise.
+//     }
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//     this.logger.log('🔌 Desconectado de la base de datos.');
+//   }
+// }
+
+
+
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  private readonly logger = new Logger(PrismaService.name);
-
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    this.logger.log('📡 Intentando conectar a la base de datos...');
-
-    try {
-      await this.$connect();
-      this.logger.log('✅ Conexión exitosa a la base de datos.');
-    } catch (error) {
-      this.logger.error('❌ Error al conectar a la base de datos:', error);
-      throw error; // Para que el deploy falle y te avise.
-    }
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
-    this.logger.log('🔌 Desconectado de la base de datos.');
+    await this.$connect();
   }
 }
