ff030f009b38784f29ba2f8ecabb2ff079f273a3
nassahel <nassahel.elias@gmail.com>
2025-03-16 01:36:46 -0300

    fix to deploy3

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,24 +1,32 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(PrismaService.name);
 
   constructor() {
     const databaseUrl = process.env.NODE_ENV === 'production'
       ? process.env.DATABASE_URL_POOLER  // En producción usa el pooler
       : process.env.DATABASE_URL;        // En local usa la conexión directa
 
-    process.env.DATABASE_URL = databaseUrl;  // Sobrescribimos DATABASE_URL
+    // Sobrescribimos DATABASE_URL antes de crear la instancia de PrismaClient
+    process.env.DATABASE_URL = databaseUrl;
 
     super();
   }
 
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      this.logger.log('Conexión exitosa a la base de datos');
+    } catch (error) {
+      this.logger.error('Error al conectar con la base de datos', error.stack);
+    }
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+    this.logger.log('Conexión a la base de datos cerrada');
   }
 }
